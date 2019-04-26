30f43c0185580d53e614f4aace7c6bc4e57c8618
OctoBrownie <46551461+OctoBrownie@users.noreply.github.com>
2019-04-25 17:20:27 -0700

    Added levels to control printing of values

--- __main__.py
+++ __main__.py
@@ -11,23 +11,25 @@ List of commands:
 	mode - finds the number(s) that occur most in the data
 	std deviation - finds the standard deviation of the list
 """	
-	def print_sort(input_list):
+	def print_sort(input_list, level):
 		print("Original list:" + str(input_list))
 		new_list = input_list
 		new_list = sorted(new_list)
-		print(new_list)
+		if level == 1:
+			print(new_list)
 		return new_list
 
-	def mean(input_list):
+	def mean(input_list, level):
 		total = 0
 		for item in input_list:
 			total = total + item
 
 		total = total / len(input_list)
-		print("mean = " + str(total))
+		if level == 1:
+			print("mean = " + str(total))
 		return total
 
-	def median(input_list):
+	def median(input_list, level):
 		input_list.sort()
 		total = input_list
 		med_index = len(input_list) / 2
@@ -35,14 +37,17 @@ List of commands:
 		if str(med_index).endswith(".5"):
 			med_index = med_index + 0.5				# to round up
 			total = total[int(med_index) - 1]
-			print(total)							# indices start at 0, not 1
+			if level == 1:
+				print("median = " + str(total))							# indices start at 0, not 1
 			return total 
 		# if number of terms is even
 		else:
 			total = (total[int(med_index) - 1] + total[int(med_index)]) / 2
-			print(total)
+			if level == 1:
+				print("median = " + str(total))
 			return total
-	def mode(input_list):
+		
+	def mode(input_list, level):
 		input_list.sort()
 		mode_num = [input_list[0], 1]		# index -1 == # of times, all others == nums 
 		last_num = [input_list[0], 1]		# the last num used
@@ -65,44 +70,48 @@ List of commands:
 		# so we don't say everything is the mode
 		if (len(mode_num[:-1])*mode_num[-1]) == len(input_list):			# mode(s) * # of times shows up == amt of #s counted
 			mode_formatted = "N/A"
+			del mode_num[0:-1]
+			mode_num = ["N/A"] + mode_num
 		else:
 			mode_formatted = "{}".format(mode_num[0])
 			for item in mode_num[1:-1]:
 				mode_formatted = mode_formatted + ", {}".format(item)
-
-		print("Mode is {0} with {1} times found.".format(mode_formatted, mode_num[-1]))
+		
+		if level == 1:
+			print("Mode is {0} with {1} times found.".format(mode_formatted, mode_num[-1]))
 		return mode_num		# returns modes (index 0 to index -2) and the amount of times found (index -1)
 		
-	def std_deviation(input_list):
+	def std_deviation(input_list, level):
 		total = 0
-		nums_squared = [] 
+		# for the one time they ask for showing work
+		# raw_num_variance = [] 
 		for item in input_list:
-			total = total + abs(item - mean)**2
-			# nums_squared.append(abs(item - mean)**2)
-			# for the one time they ask for showing work
-
+			total = total + abs(item - mean(input_list, level=2))**2
+			# raw_num_variance.append(abs(item - mean(input_list, level=2)))
+		# print(raw_num_variance)
 		total = total / len(input_list)
 		total = math.sqrt(total)
-		print("std deviation = " + str(total))
+		if level == 1:
+			print("std deviation = " + str(total))
 		return total
 	
 	if inputs == 'help':
 		print(stats_calc.__doc__)
 		return 0
+	elif type(inputs) is not list:
+		print("ERROR: Inputs were not a list.")
+		return 1
 	elif inputs == []:
-		print("Empty list.")
+		print("ERROR: Empty list.")
 		return 1
 	elif not(all(type(item) is int for item in inputs)):
-		print("Invalid input list.")
+		print("ERROR: Invalid input list.")
 		return 1
 	
 	functions = {'sort':print_sort, 'mean':mean, 'median':median, 'std deviation':std_deviation, 'mode':mode}
 	for arg in args:
 		if arg in list(functions.keys()):
-			if arg == 'sort':
-				functions[arg](inputs, False)
-			else:
-				functions[arg](inputs)
+			functions[arg](inputs, level=1)
 		else:
 			print("ERROR: Unknown function.")
 			print("Type in 'stats_calc()' or 'stats_calc('help')' for a list of valid commands.")

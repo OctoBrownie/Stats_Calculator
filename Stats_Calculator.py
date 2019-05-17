eb37296e2e53accc244d9cfc29c9e2e0a51ea1af
OctoBrownie <46551461+OctoBrownie@users.noreply.github.com>
2019-05-16 18:26:17 -0700

    Fixed range function issue (in print statement)

--- Stats_Calculator.py
+++ Stats_Calculator.py
@@ -87,7 +87,7 @@ List of commands:
 	def my_range(input_list, level):
 			input_list.sort()
 			if level == 1:
-				print("Range {0}. \nLowest val = {1} \nHighest val = {2}".format(input_list[0] - input_list[-1], 
+				print("Range {0}. \nLowest val = {1} \nHighest val = {2}".format(input_list[-1] - input_list[0], 
 																				 input_list[0], input_list[-1]))
 			return (input_list[0], input_list[-1], input_list[-1] - input_list[0])		# gives (low, high, range)
 	
